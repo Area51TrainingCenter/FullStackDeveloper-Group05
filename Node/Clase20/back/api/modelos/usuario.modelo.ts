1ee9c00abce86131d50c3c1811e2d78010632c38
sergio <sergiohidalgocaceres@gmail.com>
2018-07-17 22:06:09 -0500

    Clase20

---
+++ Node/Clase20/back/api/modelos/usuario.modelo.ts
@@ -0,0 +1,19 @@
+import mongoose = require("mongoose")
+
+const esquema = new mongoose.Schema({
+	correo: {
+		type: String,
+		unique: true,
+		trim: true,
+		required: true
+	},
+	contrasena: {
+		type: String,
+		trim: true,
+		required: true
+	}
+})
+
+const Usuario = mongoose.model("Usuario", esquema)
+
+export { Usuario }
---
+++ Node/Clase20/back/rutas/usuario.ruta.ts
@@ -0,0 +1,13 @@
+import express = require("express")
+import { controlador } from "../api/controladores/usuario.controlador"
+import { manejador as errores } from "../manejadores/errores.manejador"
+
+const ruteador = express.Router()
+
+ruteador.post("/", errores.cacheo(controlador.insertar))
+ruteador.put("/:id", errores.cacheo(controlador.actualizar))
+ruteador.get("/:id", errores.cacheo(controlador.detallar))
+ruteador.get("/", errores.cacheo(controlador.listar))
+ruteador.delete("/:id", errores.cacheo(controlador.eliminar))
+
+export { ruteador }
